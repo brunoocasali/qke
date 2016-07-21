de7386808c0cafb5b3b0580f584a220b36087a37
Bruno Casali <brunoocasali@gmail.com>
2016-07-20 23:03:03 -0300

    added code to data repository and new file to spec/support

--- lib/helpers/data_repository.rb
+++ lib/helpers/data_repository.rb
@@ -1,1 +1,27 @@
-data_repository.rb
+# frozen_string_literal: true
+class DataRepository
+  ROOT_PATH = 'data/data.log'.freeze
+
+  attr_accessor :path, :text_data
+
+  def initialize(path = ROOT_PATH)
+    @path = path
+    @text_data = []
+  end
+
+  def read
+    if file?
+      File.open(@path, 'r').each do |line|
+        @text_data << line
+      end
+    end
+  end
+
+  def file?
+    File.extname(@path) == '.log' if File.exist?(@path)
+  end
+
+  def data_directory?
+    File.dirname(@path) == 'data'
+  end
+end
